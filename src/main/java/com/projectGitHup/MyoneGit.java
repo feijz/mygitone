e5c7aefd12171f07503f3282ae01e02c6d64de6e
feijz <zhangjf373930054@163.com>
2020-11-09 17:08:31 +0800

    第一次使用gie，第二次上传

--- src/main/java/com/projectGitHup/MyoneGit.java
+++ src/main/java/com/projectGitHup/MyoneGit.java
@@ -3,5 +3,6 @@ package com.projectGitHup;
 public class MyoneGit {
     public static void main(String[] args) {
         System.out.println("第一次使用Git.....");
+        System.out.println("第2次上传Git.....");
     }
 }
